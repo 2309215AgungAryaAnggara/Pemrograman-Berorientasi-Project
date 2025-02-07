2f30fce5ae34866272eeb146c86333dc150c10b4
2309215AgungAryaAnggara <agungaryaa3@upi.edu>
2025-02-07 14:09:11 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hallo Word, Saya agung arya anggara dengan nim 2309215, Saya Senang Belajar Java");
-    }
-}
